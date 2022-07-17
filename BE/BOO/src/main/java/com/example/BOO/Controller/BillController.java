db06205cb3370948dcafa0806b1571e5ce16d179
karolrefati <karol.refati@kettydo.com>
2022-07-18 00:09:46 +0200

    updates

--- BE/BOO/src/main/java/com/example/BOO/Controller/BillController.java
+++ BE/BOO/src/main/java/com/example/BOO/Controller/BillController.java
@@ -2,6 +2,8 @@ package com.example.BOO.Controller;
 
 import com.example.BOO.Exception.ResourceNotFoundException;
 import com.example.BOO.Model.Bill;
+import com.example.BOO.Model.BillDTOResponse;
+import com.example.BOO.Model.BillProduct;
 import com.example.BOO.Repository.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -62,30 +65,42 @@ public class BillController {
 
     @GetMapping("/query")
     public ResponseEntity<?> getBillsBetweenDate(@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd")Date from , @RequestParam(required = false)@DateTimeFormat(pattern = "yyyy-MM-dd") Date to){
+
+        List<Bill> bills ;
         if (from !=null && to!=null && from.after(to)){
             throw new ResourceNotFoundException("From date should be before after") ;
         }else{
             if(from == null && to == null){
 
-                List<Bill> bills =billRepository.findAllBetweenDate(formatter.format(new Date()), formatter.format(new Date()) ) ;
-                return new ResponseEntity<>(bills , HttpStatus.OK) ;
+                bills =billRepository.findAllBetweenDate(formatter.format(new Date()), formatter.format(new Date()) ) ;
+//                return new ResponseEntity<>(bills , HttpStatus.OK) ;
             }
-            if (from == null){
-                List<Bill> bills =billRepository.findAllBetweenDate(formatter.format(to), formatter.format(to) ) ;
-                return new ResponseEntity<>(bills , HttpStatus.OK) ;
+            else if (from == null && to != null ){
+                bills =billRepository.findAllBetweenDate(formatter.format(to), formatter.format(to) ) ;
+//                return new ResponseEntity<>(bills , HttpStatus.OK) ;
             }
 
-            if (to == null){
-                List<Bill> bills =billRepository.findAllBetweenDate(formatter.format(from), formatter.format(new Date()) ) ;
-                return new ResponseEntity<>(bills , HttpStatus.OK) ;
+            else if (from != null  && to == null){
+                bills =billRepository.findAllBetweenDate(formatter.format(from), formatter.format(new Date()) ) ;
+//                return new ResponseEntity<>(bills , HttpStatus.OK) ;
             }
             else{
-                List<Bill> bills =billRepository.findAllBetweenDate(formatter.format(from), formatter.format(to) ) ;
-                return new ResponseEntity<>(bills , HttpStatus.OK) ;
+                bills =billRepository.findAllBetweenDate(formatter.format(from), formatter.format(to) ) ;
+//                return new ResponseEntity<>(bills , HttpStatus.OK) ;
             }
 
-        }
 
+            List<BillDTOResponse> billDTOResponses = new ArrayList<>() ;
+            for(Bill bill : bills ){
+                Double totalPrice = 0.0 ;
+               for(BillProduct billProduct : bill.getBillProducts()){
+                   totalPrice+= billProduct.getPrice()* billProduct.getAmount() ;
+               }
+               billDTOResponses.add(new BillDTOResponse(bill.getId(), bill.getCreatedTime(), totalPrice)) ;
+            }
+
+            return new ResponseEntity<>(billDTOResponses , HttpStatus.OK) ;
+        }
 
     }
 
---
+++ BE/BOO/src/main/java/com/example/BOO/Model/BillDTOResponse.java
@@ -0,0 +1,42 @@
+package com.example.BOO.Model;
+
+import java.util.Date;
+
+public class BillDTOResponse {
+    private int id;
+    private Date createdTime;
+    private Double totalPrice ;
+
+    public BillDTOResponse() {
+    }
+
+    public BillDTOResponse(int id, Date createdTime, Double totalPrice) {
+        this.id = id;
+        this.createdTime = createdTime;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
